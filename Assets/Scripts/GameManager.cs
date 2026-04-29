e9972b5820211b8b928864f4716a42c6412f42a1
sowTecsup <jneyra@tecsup.edu.pe>
2026-04-29 12:12:59 -0500

    Priority queue

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,46 +1,68 @@
 using UnityEngine;
 using Sirenix.OdinInspector;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    public MyQueue<string> BankQueue = new();
+    //public MyQueue<string> BankQueue = new();
+    public static GameManager instance;
 
-    public float speed = 0;
-    public float MaxSpeed = 100;
+    public PriorityQueue<Entity> priorityQueue =
+        new(  (a,b) => a.stats.speed > b.stats.speed  );
+
+    public List<Entity> entities = new();
+
+    private void Awake()
+    {
+        instance = this;    
+    }
 
 
-    public PriorityQueue<EntityStats> priorityQueue =
-        new(  (a,b) => a.speed < b.speed  );
     void Start()
     {
         
     }
-    [Button]
-    public void Enqueue(string name)
+    public void AutoAdd(Entity entity)
     {
-        BankQueue.Enqueue(name);
+        entities.Add(entity);
+    }
+    [Button]
+    public void EnqueueAll()
+    {
+        foreach (var entity in entities)
+        {
+            Enqueue(entity);
+        }
+    }
+    [Button]
+    public void Enqueue(Entity entity)
+    {
+        priorityQueue.Enqueue(entity);
     }
     [Button]
     public void Dequeue()
     {
-       Debug.Log("Pase a ser atendido : " +  BankQueue.Dequeue());
+        priorityQueue.Peek().gameObject.transform.position = Vector3.up * priorityQueue.Count * 1.5f;
+        Debug.Log("Pase a ser atendido : " + priorityQueue.Dequeue().stats.entityName);
+
+      
     }
     [Button]
     public void Peek()
     {
-        Debug.Log("El siguiente en ser atendido sera ... " + BankQueue.Peek());
+        Debug.Log("El siguiente en ser atendido sera ... " + priorityQueue.Peek().stats.entityName);
     }
 
     [Button]
     public void Clear()
     {
-        BankQueue.Clear();  
+        priorityQueue.Clear();  
     }
 
     [Button]
     public void Count()
     {
-        Debug.Log( BankQueue.Count);
+        Debug.Log(priorityQueue.Count);
     }
 
 
--- Assets/Scripts/Queues/EntityStats.cs
+++ Assets/Scripts/Queues/EntityStats.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EntityStats", menuName = "Scriptable Objects/EntityStats")]
 public class EntityStats : ScriptableObject
 {
+    public string entityName;
     public float speed;//->mayor velocidad
     public float id;//->menor id
 }
